0f335277ea6b937c318001959bd085ef4dc529cd
Your Name <you@example.com>
2021-11-18 23:27:03 -0500

    test

---
+++ soundboard.js
@@ -0,0 +1,14 @@
+function play1() {
+var audio1 = document.getElementById("audio1");
+audio1.play();
+}
+
+function play2() {
+var audio2 = document.getElementById("audio2");
+audio2.play();
+}
+
+function play3() {
+var audio3 = document.getElementById("audio3");
+audio3.play();
+}
