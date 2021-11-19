d48ceadcb0bb7d1add2d28b56d477a3be34d0bc9
Your Name <you@example.com>
2021-11-18 22:34:09 -0500

    test

--- soundboard.js
+++ soundboard.js
@@ -4,6 +4,6 @@
 
 var buttonElement = document.getElementById('my-first-button')
 
-buttonElement.addEventListener('click', function () {
-    console.log('I was clicked!')
+buttonElement.addEventListener('click', mouseenter() {
+    
   })
