40d7c4003bd339ff7bc8ccd44de7f7e47905afa5
Your Name <you@example.com>
2021-11-18 22:28:23 -0500

    button

---
+++ soundboard.js
@@ -0,0 +1,9 @@
+<button id="my-first-button">
+    I'm a button!
+</button>
+
+var buttonElement = document.getElementById('my-first-button')
+
+buttonElement.addEventListener('click', function () {
+    console.log('I was clicked!')
+  })
